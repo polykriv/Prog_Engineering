a8679f8b6f76812816cedcd0fb6b70bce78791ef
polykriv <161135983+polykriv@users.noreply.github.com>
2025-10-18 15:45:38 +0500

    Delete Kod directory

--- Kod/T5N1.py
+++
@@ -1,4 +0,0 @@
-set_1 = {'White', 'Black', 'Red', 'Pink'}
-set_2 = {'Red', 'Green', 'Blue', 'Red'}
-
-print(set_1 - set_2)
--- Kod/T5N6.py
+++
@@ -1,7 +0,0 @@
-superhoroes = ['superman', 'spiderman', 'batman']
-
-nikolay, vasiliy, ivan = superhoroes
-
-print('Николай -', nikolay)
-print('Василий -', vasiliy)
-print('Иван -', ivan)
