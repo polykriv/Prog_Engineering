e5296550b58bd52e66727cafcb106e0d3b3607ea
polykriv <161135983+polykriv@users.noreply.github.com>
2025-11-01 15:55:09 +0500

    Delete Kod directory

--- Kod/T7N6.py
+++
@@ -1,6 +0,0 @@
-with open('input.txt', 'a+') as f:
-    f.write('\nNew line')
-
-with open ('input.txt', 'r') as f:
-    result = f.readlines()
-    print(result)
--- Kod/T7N7.py
+++
@@ -1,5 +0,0 @@
-lines = ['one', 'two', 'three']
-with open('input.txt', 'w') as f:
-    for line in lines:
-        f.write('\nLa La La ' + line)
-    print('Done!')
